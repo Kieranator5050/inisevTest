94e30d5a802f754342c72555f2d1acca42e73d9b
Kieran Jaggernauth <thejugg63@gmail.com>
2022-07-25 12:27:55 -0500

    Updated post validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,22 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use App\Models\Post;
 
 class PostController extends Controller
 {
     //
-    public function store()
+    public function store(Request $request)
     {
         //Assuming a website id is provided or obtained when a request is sent
-        $attributes = request()->validate([
+        $validator = Validator::make($request->all(),[
             'website_id'=>[Rule::exists('websites','id')],
             'title'=>['required'],
             'description'=>['required']
         ]);
 
-        Post::create($attributes);
+        if($validator->fails())
+        {
+            return response()->json($validator->errors());
+        }
+
+        Post::create([
+            'website_id'=>$request['website_id'],
+            'title'=>$request['title'],
+            'description'=>$request['description']
+        ]);
 
         return ['success'=>'Post Created'];
     }
